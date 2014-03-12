25dfd8d2dd779b9700c7e5f7650734b91272870b
Doug Grainger <dhgrainger@gmail.com>
2014-03-12 17:27:19 -0400

    Refactoring with Adam

--- wizard.rb
+++ wizard.rb
@@ -5,35 +5,41 @@ require 'pry'
 require_relative'article_class.rb'
 
 
-articles = Hash.new
-
-def read(file, articles)
-CSV.foreach(file, headers: true) do |row|
-	rows = row.to_hash
-	articles.store(row["title"], {url: rows["url"], article: rows["article"]})
+def read(file)
+  articles = Hash.new
+  CSV.foreach(file, headers: true) do |row|
+  	rows = row.to_hash
+  	articles.store(rows["url"], {title: rows["title"], article: rows["article"]})
+  end
+  articles
 end
-articles
-end
 
-read('article.csv', articles)
+
 
 post '/article' do
 	article = WizardArticle.new(params['title'], params['url'], params['article'])
 	article.write('article.csv')
-	articles = {}
-	read('article.csv', articles)
+	articles = read('article.csv')
 	redirect '/'
 end
 
 get '/' do
-   @articles = articles
+   @articles = read('article.csv')
    erb :index
 end
 
- articles.each do |title, hash|
-  get "/#{URI::encode(hash[:url])}" do
-    @header = title
-    @article = hash[:article]
-    erb :wizard_news
-  end
+# articles = read('article.csv')
+get '/:url' do
+  @articles = read('article.csv')
+  @header = @articles[params[:url]][:title]
+  @article = @articles[params[:url]][:article]
+  erb :wizard_news
 end
+
+#  articles.each do |title, hash|
+#   get "/#{URI::encode(hash[:url])}" do
+#     @header = title
+#     @article = hash[:article]
+#     erb :wizard_news
+#   end
+# end
